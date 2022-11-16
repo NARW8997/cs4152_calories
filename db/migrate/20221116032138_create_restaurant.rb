8249e10d6e854e17dcf9ee1da8136d56e758d4e6
WenshuoXie/restaurant-microservice <wxie48@wisc.edu>
2022-11-15 23:46:42 -0500

    Established restaurants table, and created some seed dat.Finished the meal plan logic.

---
+++ db/migrate/20221116032138_create_restaurant.rb
@@ -0,0 +1,12 @@
+class CreateRestaurant < ActiveRecord::Migration
+  def change
+    create_table :restaurants, id: false do |t|
+      t.string :name
+      t.primary_key :id
+      t.string :cal_type
+      t.string :location
+
+      t.timestamps null: false
+    end
+  end
+end
